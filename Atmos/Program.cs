2f130dc42f746abd5f50b4c80b5fbcf981d2bb6e
Chris Smith <csmithut@gmail.com>
2018-01-22 14:34:23 -0700

    Added project files

---
+++ Atmos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Atmos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
